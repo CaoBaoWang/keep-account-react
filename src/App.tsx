346eb141a5071fdb17d545b7ee980d3d7b474d41
wangshaoli <1125550225@qq.com>
2021-05-16 20:13:48 +0800

    nav and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,61 @@
 import React from 'react'
+import './index.scss'
 import Tags from "./views/Tags";
 import Money from "./views/Money";
 import Statistics from "./views/Statistics";
 import NoMatch from "./views/NoMatch";
+import Nav from './components/Nav.tsx'
 
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Switch,
     Route,
     Redirect,
     Link
 } from "react-router-dom";
+import styled from "styled-components";
+import {isPrimitive} from "util";
+
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+    flex-grow: 1;
+`
+
+
 
 function App() {
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-
-                        <li>
-                            <Link to="/tags">tags</Link>
-                        </li>
-                        <li>
-                            <Link to="/money">money</Link>
-                        </li>
-                        <li>
-                            <Link to="/statistics">Statistics</Link>
-                        </li>
-                    </ul>
-                </nav>
-
+            <Wrapper>
+                <Main>
                 <Switch>
-                    <Redirect exact from="/" to="/money" />
+                    <Redirect exact from="/" to="/money"/>
                     <Route path="/tags">
-                        <Tags />
+                        <Tags/>
                     </Route>
                     <Route path="/money">
-                        <Money />
+                        <Money/>
                     </Route>
                     <Route path="/statistics">
-                        <Statistics />
+                        <Statistics/>
                     </Route>
                     <Route path="*">
                         <NoMatch/>
                     </Route>
                 </Switch>
-            </div>
+                </Main>
+                <Nav />
+            </Wrapper>
         </Router>
     );
 }
-
-
-
-
-
-
-
 
 
 export default App
