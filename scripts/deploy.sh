4423e39c2d6a039bd3feaa1a9f9682290ccae4da
wangshaoli <1125550225@qq.com>
2021-05-29 17:25:38 +0800

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,5 @@ git init &&
 git add . &&
 git commit -m 'deploy' &&
 git remote add origin git@github.com:CaoBaoWang/keep-account-website.git &&
-git push origin master -f
+git push origin master -f &&
 cd -
